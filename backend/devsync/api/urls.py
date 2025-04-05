13b017303213939ee2fc47ebef4947dfc665c67a
TheMiner <99322828+TheMineron@users.noreply.github.com>
2025-04-05 21:32:11 +0700

    feature: create user

--- backend/devsync/api/urls.py
+++ backend/devsync/api/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path, include, re_path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.index),
     path("test-mail", views.send_test_mail),
     path("v1/drf-auth/", include("rest_framework.urls")),
-    path(r"v0/auth/", include("djoser.urls.authtoken")),
     path("v0/", include("djoser.urls")),
+    path(r"v0/auth/", include("djoser.urls.authtoken")),
     path("v1/users/", include("users.urls")),
 ]
