a7e002677e068ba819617def8c3899a6f0ae76e1
TheMiner <99322828+TheMineron@users.noreply.github.com>
2025-04-07 15:26:14 +0700

    fix: urls

--- backend/devsync/api/urls.py
+++ backend/devsync/api/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("test-mail", views.send_test_mail),
-    path("v1/drf-auth/", include("rest_framework.urls")),
-    path("v0/", include("djoser.urls")),
-    path(r"v0/auth/", include("djoser.urls.authtoken")),
     path("v1/users/", include("users.urls")),
+    path("v1/", include("djoser.urls")),
+    path(r"v1/auth/", include("djoser.urls.authtoken")),
 ]
