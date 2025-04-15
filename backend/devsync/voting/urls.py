9682b3b4536ecc4df5f409eae930210288ada299
chernylovelas <elina.vv.2003@gmail.com>
2025-04-15 18:34:13 +0700

    add voting api urls

--- backend/devsync/voting/urls.py
+++ backend/devsync/voting/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from . import views
 from .views import VotingViewSet, VotingOptionViewSet, VotingOptionChoiceViewSet
 
 router = DefaultRouter()
