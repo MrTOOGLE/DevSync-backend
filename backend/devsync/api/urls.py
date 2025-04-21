24e5683a775a41908ea1667b0812608737301c4e
chernylovelas <elina.vv.2003@gmail.com>
2025-04-21 16:31:34 +0700

    voting api

--- backend/devsync/api/urls.py
+++ backend/devsync/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index),
     path("v1/users/", include("users.urls")),
-    path("v1/voting/", include("voting.urls")),
+    path("v1/", include("voting.urls")),
     path("v1/", include("projects.urls")),
     path(r"v1/auth/", include("djoser.urls.authtoken")),
 ]
--- backend/devsync/voting/urls.py
+++ backend/devsync/voting/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path, include
+from rest_framework_nested import routers
+from .views import VotingViewSet, VotingOptionViewSet, VotingOptionChoiceViewSet, VotingCommentViewSet
 from rest_framework.routers import DefaultRouter
 
-from .views import VotingViewSet, VotingOptionViewSet, VotingOptionChoiceViewSet, VotingCommentViewSet
-
 router = DefaultRouter()
-router.register(r'', VotingViewSet, basename='voting')
-router.register(r'option', VotingOptionViewSet, basename='voting-option')
-router.register(r'choice', VotingOptionChoiceViewSet, basename='voting-choice')
-router.register(r'comment', VotingCommentViewSet, basename='voting-comment')
+router.register(r'votings', VotingViewSet, basename='voting')
+
+voting_router = routers.NestedDefaultRouter(router, r'votings', lookup='voting')
+voting_router.register(r'options', VotingOptionViewSet, basename='voting-option')
+voting_router.register(r'choices', VotingOptionChoiceViewSet, basename='voting-choice')
+voting_router.register(r'comments', VotingCommentViewSet, basename='voting-comment')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(voting_router.urls)),
 ]
