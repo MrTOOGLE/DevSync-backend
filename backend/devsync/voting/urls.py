02d8468df3a7d9a36a8db790b9c86de586f32b68
TheMiner <99322828+TheMineron@users.noreply.github.com>
2025-04-29 16:47:56 +0700

    url error fix

--- backend/devsync/voting/urls.py
+++ backend/devsync/voting/urls.py
@@ -6,14 +6,10 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'projects/(?P<project_pk>[^/.]+)/votings', VotingViewSet, basename='voting')
 
-voting_router = routers.NestedDefaultRouter(router, r'projects/(?P<project_pk[^/.]+)/votings', lookup='voting')
-voting_router.register(r'options', VotingOptionViewSet, basename='voting-option')
-
-voting_router = routers.NestedDefaultRouter(router, r'votings', lookup='voting')
+voting_router = routers.NestedDefaultRouter(router, r'projects/(?P<project_pk>[^/.]+)/votings', lookup='voting')
 voting_router.register(r'options', VotingOptionViewSet, basename='voting-option')
 voting_router.register(r'choices', VotingOptionChoiceViewSet, basename='voting-choice')
 voting_router.register(r'comments', VotingCommentViewSet, basename='voting-comment')
-
 
 urlpatterns = [
     path('', include(router.urls)),
