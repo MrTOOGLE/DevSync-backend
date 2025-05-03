489bce3116cd7353bb122dfd8d6824eaf07d1b52
TheMiner <99322828+TheMineron@users.noreply.github.com>
2025-05-03 23:00:03 +0700

    add admin models

--- backend/devsync/roles/admin.py
+++ backend/devsync/roles/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from roles.models import Role
+from roles.models import Role, RolePermission, MemberRole
 
 
 @admin.register(Role)
@@ -10,3 +10,14 @@ class RoleAdmin(admin.ModelAdmin):
     list_filter = ("project", )
 
     save_on_top = True
+
+
+@admin.register(RolePermission)
+class RolePermissionAdmin(admin.ModelAdmin):
+    list_display = ('codename', 'name', 'category', 'description')
+    search_fields = ['codename']
+
+
+@admin.register(MemberRole)
+class MemberRoleAdmin(admin.ModelAdmin):
+    list_display = ('role', 'user')
