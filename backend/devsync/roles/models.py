94c07ec2d1fba7ef82b50116363b749494ec8ba7
TheMiner <99322828+TheMineron@users.noreply.github.com>
2025-05-03 22:53:13 +0700

    add role permissions model

--- backend/devsync/roles/models.py
+++ backend/devsync/roles/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-from django.utils.timezone import now
 
 User = get_user_model()
 
@@ -11,6 +10,8 @@ class Role(models.Model):
     project = models.ForeignKey("projects.Project", related_name='roles', on_delete=models.CASCADE)
     color = models.CharField(max_length=7, default="#000000")
     rank = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(100)])
+    permissions = models.ManyToManyField("RolePermission")
+    is_everyone = models.BooleanField(default=False)
 
     def __str__(self):
         return f'Role {self.name} (id: {self.id})'
@@ -22,30 +23,14 @@ class MemberRole(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.role} for {self.user}'
+        return f'{self.role} ({self.user})'
 
 
-"""class RolePermissions(models.Model):
-    role = models.OneToOneField(Role, related_name='permissions', on_delete=models.CASCADE)
-    all_permissions = models.BooleanField(default=False)
-    manage_project = models.BooleanField(default=False)
-    manage_members = models.BooleanField(default=False)
-    manage_members_in_department = models.BooleanField(default=False)
-    manage_roles = models.BooleanField(default=False)
-    manage_roles_in_department = models.BooleanField(default=False)
-    assign_roles = models.BooleanField(default=False)
-    assign_roles_in_department = models.BooleanField(default=False)
-    manage_departments = models.BooleanField(default=False)
-    manage_department = models.BooleanField(default=False)
-    manage_tasks = models.BooleanField(default=False)
-    manage_tasks_in_department = models.BooleanField(default=False)
-    add_tasks = models.BooleanField(default=True)
-    add_tasks_in_department = models.BooleanField(default=True)
-    manage_votings = models.BooleanField(default=False)
-    add_voting = models.BooleanField(default=True)
-    vote_in_voting = models.BooleanField(default=True)
-    manage_comments = models.BooleanField(default=False)
-    view_audit = models.BooleanField(default=False)
+class RolePermission(models.Model):
+    codename = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    description = models.TextField(max_length=1256)
 
     def __str__(self):
-        return f"Permissions for {self.role.name} ({self.role.project})"""
+        return f"{self.name} ({self.codename})"
